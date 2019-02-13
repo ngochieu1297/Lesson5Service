7ae031bffd9a303481520a0e2db6db6bda251777
Admin <ngochieu1297@gmail.com>
2019-02-13 15:39:52 +0700

    main_func_1

--- app/src/main/java/com/example/admin/lesson5service/OnItemClickListener.java
+++ app/src/main/java/com/example/admin/lesson5service/OnItemClickListener.java
@@ -3,5 +3,5 @@ package com.example.admin.lesson5service;
 import android.view.View;
 
 public interface OnItemClickListener {
-    void onItemClick(View view, int position);
+    void onItemClick(int position);
 }
